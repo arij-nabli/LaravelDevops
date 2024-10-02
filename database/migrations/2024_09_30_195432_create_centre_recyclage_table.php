122809224e2d7bd20dd43f109e12b348123e97f2
Emna <emna.felfel@esprit.tn>
2024-10-02 21:36:37 +0100

    finition

--- database/migrations/2024_09_30_195432_create_centre_recyclage_table.php
+++ database/migrations/2024_09_30_195432_create_centre_recyclage_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('centres_recyclage', function (Blueprint $table) {
+        Schema::create('centre_recyclages', function (Blueprint $table) {
             $table->id();
             $table->string('nom');
             $table->string('adresse');
